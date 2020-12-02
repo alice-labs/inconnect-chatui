fd05c1e6cc36135f4618235970a5c6f91f1e101f
Rafiqul Hasan <shopnilsazal@gmail.com>
2020-12-03 01:36:12 +0600

    chore: adds carousel to gallery message

--- src/components/Gallery/GalleryMessage.stories.tsx
+++ src/components/Gallery/GalleryMessage.stories.tsx
@@ -20,61 +20,134 @@ export const Example = () => (
       consumer={'user'}
       hasTitle={true}
       avatar='https://misx-assets.s3-ap-southeast-1.amazonaws.com/robot.svg'
-      galleryData={[{
+      galleryData={[
+        {
           id: 0,
           title: 'Pureit Mineral Ultima RO + UV',
           subtitle: '2500$',
-          image: 'https://images.unsplash.com/photo-1606830079880-e90bd08e9a98?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80',
-          buttons:[{
+          image:
+            'https://images.unsplash.com/photo-1606830079880-e90bd08e9a98?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80',
+          buttons: [
+            {
               id: 1,
               title: 'Button 01',
               methodType: 'url',
               url: 'https://getalice.ai',
               isDisabled: false,
-          },{
+            },
+            {
               id: 2,
               title: 'Button 02',
               methodType: 'function',
-              action:() => console.log('Button 02 clicked'),
+              action: () => console.log('Button 02 clicked'),
               isDisabled: false,
-          },{
+            },
+            {
               id: 3,
               title: 'Button 03',
               methodType: 'function',
-              action:() => console.log('Button 02 clicked'),
+              action: () => console.log('Button 02 clicked'),
               isDisabled: true,
-          }]
-      },{
+            },
+          ],
+        },
+        {
           id: 1,
           title: 'Random Dear Visit',
           subtitle: '150$',
-          image: 'https://images.unsplash.com/photo-1606762803100-5b4833aaccc0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1633&q=80',
-          buttons:[{
+          image:
+            'https://images.unsplash.com/photo-1606762803100-5b4833aaccc0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1633&q=80',
+          buttons: [
+            {
               id: 1,
               title: 'Button 01',
               methodType: 'url',
               url: 'https://getalice.ai',
               isDisabled: false,
-          },{
+            },
+            {
               id: 2,
               title: 'Button 02',
               methodType: 'function',
-              action:() => console.log('Button 02 clicked'),
+              action: () => console.log('Button 02 clicked'),
               isDisabled: false,
-          },{
+            },
+            {
               id: 3,
               title: 'Button 03',
               methodType: 'function',
-              action:() => console.log('Button 02 clicked'),
+              action: () => console.log('Button 02 clicked'),
               isDisabled: false,
-          }]
-      }]}
+            },
+          ],
+        },
+        {
+          id: 2,
+          title: 'Random Visit',
+          subtitle: '150$',
+          image:
+            'https://images.unsplash.com/photo-1606762803100-5b4833aaccc0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1633&q=80',
+          buttons: [
+            {
+              id: 1,
+              title: 'Button 01',
+              methodType: 'url',
+              url: 'https://getalice.ai',
+              isDisabled: false,
+            },
+            {
+              id: 2,
+              title: 'Button 02',
+              methodType: 'function',
+              action: () => console.log('Button 02 clicked'),
+              isDisabled: false,
+            },
+            {
+              id: 3,
+              title: 'Button 03',
+              methodType: 'function',
+              action: () => console.log('Button 02 clicked'),
+              isDisabled: false,
+            },
+          ],
+        },
+        {
+          id: 3,
+          title: 'Random Dear',
+          subtitle: '150$',
+          image:
+            'https://images.unsplash.com/photo-1606762803100-5b4833aaccc0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1633&q=80',
+          buttons: [
+            {
+              id: 1,
+              title: 'Button 01',
+              methodType: 'url',
+              url: 'https://getalice.ai',
+              isDisabled: false,
+            },
+            {
+              id: 2,
+              title: 'Button 02',
+              methodType: 'function',
+              action: () => console.log('Button 02 clicked'),
+              isDisabled: false,
+            },
+            {
+              id: 3,
+              title: 'Button 03',
+              methodType: 'function',
+              action: () => console.log('Button 02 clicked'),
+              isDisabled: false,
+            },
+          ],
+        },
+      ]}
     />
     {/*<GalleryMessage*/}
-      {/*text='hello user this are our product list'*/}
-      {/*msgTime={'4h ago'}*/}
-      {/*consumer={'admin'}*/}
-      {/*avatar='https://misx-assets.s3-ap-southeast-1.amazonaws.com/robot.svg'*/}
+    {/*text='hello user this are our product list'*/}
+    {/*msgTime={'4h ago'}*/}
+    {/*consumer={'admin'}*/}
+    {/*avatar='https://misx-assets.s3-ap-southeast-1.amazonaws.com/robot.svg'*/}
     {/*/>*/}
   </div>
 );
